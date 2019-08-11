2571d5dd64d42a3046b8da13233bf9c982bc18eb
Ryan Levitt <rlevitt97@gmail.com>
2019-08-11 06:14:31 -0400

    website shell working

---
+++ pelicanconf.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Ryan Levitt'
+SITENAME = 'Ryan Levitt\'s Coding Blog'
+SITEDESCRIPTION = 'Projects.'
+SITEURL = 'http://localhost:8000'
+
+PATH = 'content'
+PAGE_PATHS = ['pages']
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+
+THEME = 'theme'
+STATIC_PATHS = ['images', 'static', 'extra']
+EXTRA_PATH_METADATA = {
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/header.jpg': {'path': 'header.jpg'},
+}
+HEADER = 'images/header.jpg'
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = (('github', 'https://github.com/rlevv'),
+          ('linkedin', 'https://www.linkedin.com/in/ryanlevv/'),
+          ('stackoverflow', 'https://stackoverflow.com/users/10782802/rlevv'))
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
